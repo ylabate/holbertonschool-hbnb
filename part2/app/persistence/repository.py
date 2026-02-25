8e190093d0c9e507c16306beea07982d13b6fbf9
ylabate <12196@holbertonstudents.com>
2026-02-25 13:38:37 +0000

    Refactor(repository): Add type hints

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -3,27 +3,27 @@ from abc import ABC, abstractmethod
 
 class Repository(ABC):
     @abstractmethod
-    def add(self, obj):
+    def add(self, obj) -> None:
         pass
 
     @abstractmethod
-    def get(self, obj_id):
+    def get(self, obj_id) -> object | None:
         pass
 
     @abstractmethod
-    def get_all(self):
+    def get_all(self) -> list[object]:
         pass
 
     @abstractmethod
-    def update(self, obj_id, data):
+    def update(self, obj_id, data) -> None:
         pass
 
     @abstractmethod
-    def delete(self, obj_id):
+    def delete(self, obj_id) -> None:
         pass
 
     @abstractmethod
-    def get_by_attribute(self, attr_name, attr_value):
+    def get_by_attribute(self, attr_name, attr_value) -> object | None:
         pass
 
 
@@ -31,24 +31,30 @@ class InMemoryRepository(Repository):
     def __init__(self):
         self._storage = {}
 
-    def add(self, obj):
+    def add(self, obj) -> None:
         self._storage[obj.id] = obj
 
-    def get(self, obj_id):
+    def get(self, obj_id) -> object | None:
         return self._storage.get(obj_id)
 
-    def get_all(self):
+    def get_all(self) -> list[object]:
         return list(self._storage.values())
 
-    def update(self, obj_id, data):
+    def update(self, obj_id, data) -> None:
         obj = self.get(obj_id)
         if obj:
             obj.update(data)
 
-    def delete(self, obj_id):
+    def delete(self, obj_id) -> None:
         if obj_id in self._storage:
             del self._storage[obj_id]
 
-    def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values()
-                     if getattr(obj, attr_name) == attr_value), None)
+    def get_by_attribute(self, attr_name, attr_value) -> object | None:
+        return next(
+            (
+                obj
+                for obj in self._storage.values()
+                if getattr(obj, attr_name) == attr_value
+            ),
+            None,
+        )
