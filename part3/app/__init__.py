708d1cdec842e1a5560bfbecd7f16e3094c07676
ylabate <12196@holbertonstudents.com>
2026-03-10 08:23:09 +0000

    feat(auth): Add JWT support and dotenv
    Defer API namespace imports into create_app to avoid circular import
    issues
    and load environment variables in run.py before creating the app

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
-
+from flask_jwt_extended import JWTManager
 bcrypt = Bcrypt()
-
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as place_ns
-from app.api.v1.reviews import api as reviews_ns
+jwt = JWTManager()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as place_ns
+    from app.api.v1.reviews import api as reviews_ns
+
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
     api = Api(
         app,
         version="1.0",
